10842306c68221b9e00c7b9cad0e763565b51d50
Duyan-Kim <arbolkim@gmail.com>
2021-01-30 22:58:10 +0900

    refactor: map을 사용하여 tab 리팩토링

--- src/App.js
+++ src/App.js
@@ -6,30 +6,66 @@ import BookContainer from "./components/BookContainer";
 import Slider from "./components/Slider";
 import BookCover from "./img/cover4.jpg";
 import SearchResult from "./components/SearchResult";
-//import img1 from "./img/kid3_resize700.jpg";
-//import MenuContainer from "./components/MenuContainer";
 import "./styles/style.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+
+const tabContents = [
+  {
+    tab: "Home",
+    content: (
+      <>
+        <SearchResult title="돼지책" author="앤서니" img={BookCover} />
+        <Slider />
+      </>
+    ),
+  },
+  {
+    tab: "BestSeller",
+    content: (
+      <>
+        <BookContainer name="소설 베스트셀러" categoryId="101" />
+        <BookContainer name="시/에세이 베스트셀러" categoryId="102" />
+      </>
+    ),
+  },
+  {
+    tab: "New",
+    content: (
+      <>
+        <BookContainer name="소설 신간" categoryId="101" />
+        <BookContainer name="시/에세이 신간" categoryId="102" />
+      </>
+    ),
+  },
+];
+
+const useTabs = (initialTab, allTabs) => {
+  const [tabIndex, setTabIndex] = useState(initialTab);
+  return {
+    contentItem: allTabs[tabIndex],
+    contentChange: setTabIndex,
+  };
+};
 
 const App = () => {
+  const { contentItem, contentChange } = useTabs(1, tabContents);
+
   return (
     <div className="container">
       <Navigation />
       <div className="nav-tabs-div">
-        <Tabs defaultActiveKey="Bestsellers">
-          <Tab eventKey="Home" title="Home">
-            <Slider />
-            <SearchResult title="돼지책" author="앤서니" img={BookCover} />
-          </Tab>
-          <Tab eventKey="Bestsellers" title="Bestsellers">
-            <BookContainer name="소설 베스트셀러" categoryId="101" />
-            <BookContainer name="시/에세이 베스트셀러" categoryId="102" />
-          </Tab>
-          <Tab eventKey="New" title="New">
-            <BookContainer name="소설 신간" categoryId="101" />
-            <BookContainer name="시/에세이 신간" categoryId="102" />
-          </Tab>
-          <Tab eventKey="My books" title="My books"></Tab>
+        <Tabs>
+          {tabContents.map((section, index) => (
+            <Tab
+              eventKey={tabContents[index].tab}
+              title={tabContents[index].tab}
+              onClick={() => contentChange(index)}
+              key={index}
+            >
+              {tabContents[index].content}
+            </Tab>
+          ))}
         </Tabs>
       </div>
       <Footer />
